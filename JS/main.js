dc43778d8d2884d99174698f0d715bc83cc8ed19
birukyemane <bir_yem@yahoo.com>
2018-09-26 18:14:04 +0300

    styling the page, improve displaying the matches found, click event handler for radio buttons

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,4 @@
-const countries= ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
-	,"Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands"
-	,"Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica"
-	,"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea"
-	,"Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana"
-	,"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India"
-	,"Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia"
-	,"Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania"
-	,"Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia"
-	,"New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal"
-	,"Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre and Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles"
-	,"Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts and Nevis","St Lucia","St Vincent","St. Lucia","Sudan"
-	,"Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad and Tobago","Tunisia"
-	,"Turkey","Turkmenistan","Turks and Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay"
-  ,"Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
   
 	function randomHexaColor(){
       let randomNum = Math.floor(Math.random()* 10000000);
@@ -31,7 +17,9 @@ const countries= ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla
 		let item;
 		let container = 	document.querySelector('#container');
 		let	numOfResults = document.createElement('p');
-		numOfResults.textContent = `Countries that start with: ${result.length}`;	
+		document.querySelector('#container').innerHTML = '';		
+		numOfResults.textContent = (result.length >1 ? `${result.length} matchs found` : `${(result.length==1?"1 match found":"No matches found")}`);	
+		(result.length >= 1? numOfResults.className="found":numOfResults.className="not-found");
 		container.appendChild(numOfResults);
 		result.forEach(element => {
 			item = document.createElement('div');
@@ -40,25 +28,22 @@ const countries= ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla
 			item.style.background = randomHexaColor();
 		});				
 	}
-
-	function clearResult(){ // clears the dom 
-		document.querySelector('#container').innerHTML = '';
-	}
 	
-	function runSearch(search,display){  
+	function runSearch(search){  
 		let keyWord = document.querySelector('#searchInput').value.toLowerCase(); 
-		let result = search(keyWord); // search 
-		clearResult(); // clear the display container div 
-		display(result); // display the reslut 		
+		if (keyWord){
+			let result = search(keyWord); // search 
+			displayResult(result); // display the reslut
+		}	 		
 	}
 
-	function handleSearchEvent(e){ // it handles the whole process of searching
+	function handleSearchEvent(){ // it handles the whole process of searching
 		let startwith = document.querySelector('#startWord').checked;
-		let Anyword = document.querySelector('#anyWord').checked;
+		let anyWord = document.querySelector('#anyWord').checked;
 		if(startwith){
-			runSearch(startsWith, displayResult);
-			} else {
-			runSearch(containsWord, displayResult);
+			runSearch(startsWith);
+			} else if(anyWord){
+			runSearch(containsWord);
 		}		
 		
 	}
@@ -66,8 +51,16 @@ const countries= ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla
    // Event handlers
 
 	document.querySelector('#searchInput').addEventListener('input',handleSearchEvent);
+	document.querySelector('#startWord').onclick = handleSearchEvent;
+	document.querySelector('#anyWord').onclick = handleSearchEvent;
 
 
 	// initial state .. when the page loads first 
 	let noOfCountries =  document.querySelector('#noOfCountries');
 	noOfCountries.append(document.createElement('textNode').value = countries.length);
+	let container = document.querySelector('#container');
+	let	initialMessage = document.createElement('h2');
+	initialMessage.textContent = "Search patterns in country names!";
+	initialMessage.className = "initialMessage";
+	container.appendChild(initialMessage);
+
